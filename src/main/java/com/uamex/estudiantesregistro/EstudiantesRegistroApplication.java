2466cb989cc3634c2cdea0523ba5b69f0dec6253
Roque Betanzos <gerardossj2@hotmail.com>
2025-08-18 10:17:44 -0600

    mi primer commit

---
+++ src/main/java/com/uamex/estudiantesregistro/EstudiantesRegistroApplication.java
@@ -0,0 +1,13 @@
+package com.uamex.estudiantesregistro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiantesRegistroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudiantesRegistroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uamex/estudiantesregistro/EstudiantesRegistroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uamex.estudiantesregistro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesRegistroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
